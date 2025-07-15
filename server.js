955c04164fb682961c7e1ac782cddbf398ae2599
Huzaifasajjad0120 <Huzaifa.Sajjad@korapoint.com>
2025-07-15 23:07:02 +0500

    aa

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 // Middleware
 const allowedOrigins = [
   'http://localhost:3000',
-  'gym-management-production-e84d.up.railway.app'
+  'https://gym-management-production-e84d.up.railway.app'
 ];
 
 app.use(cors({
