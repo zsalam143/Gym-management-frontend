8974a4f6d7f55b0b0ea06064eaf51405090cd874
Huzaifasajjad0120 <Huzaifa.Sajjad@korapoint.com>
2025-07-15 22:45:13 +0500

    add

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors({
-  origin: '*', // <-- your frontend origin
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 app.use(bodyParser.json());
